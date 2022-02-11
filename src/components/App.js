255db9dcfcf582852eec8c9a1f383968b4a22765
mattlandry237 <mattlandry237@gmail.com>
2022-02-11 16:06:58 -0700

    added embedded video & default search

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,20 @@ import youtube from "../apis/youtube";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("pokemon");
+  }
+
   onTermSubmit = async (term) => {
-    const response = await await youtube.get("/search", {
+    const response = await youtube.get("/search", {
       params: { q: term },
     });
 
-    this.setState({ videos: response.data.items });
-    this.setState({ selectedVideo: response.data.items[0] });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
@@ -23,11 +30,19 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelect={this.onVideoSelect}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,7 +4,11 @@ import React from "react";
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div className="item video-item" onClick={() => onVideoSelect(video)}>
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img
+        className="ui image"
+        alt={video.snippet.title}
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
